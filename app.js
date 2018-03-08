13caccff7dba23887d84827fc793c5a242e818d5
ray <alien1983gs@126.com>
2018-03-08 18:40:03 +0800

    优化登录逻辑

--- app.js
+++ app.js
@@ -1,159 +1,113 @@
 //app.js
 App({
-  onLaunch: function () {
-    
+  onLaunch: () => {
+
     // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
+    const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
+    // 展示用户当前存储在本地的第三方 session_key
+    const local_session_key = wx.getStorageSync(`local_session_key`);
+    console.log(`用户当前存储在本地的第三方 session_key（local_session_key）: %c${local_session_key}`,`color: #ffff; background-color: #113c6d;`);
+
+    // 进入小程序就让用户登录
     wx.login({
+      // 访问微信的 login 接口成功后就开始执行登录逻辑
       success: res => {
-        // 请求微信服务器成功拿到用户的code(有效期五分钟)
-        console.log(`并从微信服务端拿到用户的code(有效期五分钟): %c${res.code}`, `color: #ff9600;`);
+        // 开始执行登录逻辑，请求微信服务器拿用户的 code (有效期五分钟)
+        console.log(`使用 we.login 从微信拿到用户的code: %c${res.code}`,`color: #ffff; background-color: #113c6d;`);
+        // 如果接口返回的数据里的 code 存在，就开始访问第三方服务器的登录接口
         if (res.code) {
+          // 请求第三放服务器的登录接口
           wx.request({
             method: 'POST',
-            // 润荣本地调试接口
-            url: 'http://192.168.7.110:3000/wxApi/login', 
+            url: 'http://192.168.7.160:3000/api/getToken', // 润荣本地调试接口
+            // 将拿到的 code 传到第三方服务器
+            // 由第三方服务器将 code 发回给微信以换取拥有这个 code 的微信用户的 session_key 和 session_id
+            // 再由第三方组装好一个新的第三方 session_key 传回给微信用户，作身份识别用途
             data: {
               code: res.code
             },
+            // 成功访问第三方登录接口后会拿到重新组装过的 session_key
             success: res => {
-              console.log(`重新登录成功，返回信息为 %c${res.errMsg}`,`color: #6ab796;`);
-              console.log(`登录接口返回数据为：`,res.data);
-              // 异步存储 session_key 到 localStorage
-              wx.setStorage({
-                key: `session_key`,
-                data: res.data.session_key,
-                success: res => {
-                  console.log(`session_key本地存储成功，返回信息为 %c${res.errMsg}`,`color: #6ab796;`);
+              // 如果接口返回的 session_key 存在，就开始完成二步登录业务逻辑
+              console.log(`第三方返回重组完成的 session_key: %c${res.data.session_key}`,`color: #ffff; background-color: #113c6d;`);
+              if (res.data.session_key) {
+                // 第一步: 同步存储拿到的第三方 session_key 到 localStorage，命名为 local_session_key
+                try {
+                  wx.setStorageSync(
+                    `local_session_key`,
+                    `${res.data.session_key}`
+                  )
+                  // 存储成功后将 local_session_key 在控制台打印出来
+                  const local_session_key = wx.getStorageSync(`local_session_key`);
+                  console.log(`执行 wx.setStorageSync 成功，local_session_key 变更为: %c${local_session_key}`,`color: #ffff; background-color: #113c6d;`);
                 }
-              })
+                catch (e) {
+                  console.log(`同步存储 session_key 到本地 local_session_key 时出错`,e);
+                }
+                // 第二步: 使用 wx.getSetting 访问微信服务器确认小程序之前是否已经申请过获取用户公开信息的授权
+                wx.getSetting({
+                  // 访问成功后，如果返回的数据里 scope.userInfo（公开信息）不存在则重新获取用户授权
+                  success: res => {
+                    if (!res.authSetting['scope.userInfo']) {
+                      console.log(`用户并未授权获取TA的公开信息，权限属性 scope.userInfo: %c${res.authSetting[`scope.userInfo`]}`,`color: #ffff; background-color: #113c6d;`);
+                      // 获取用户授权拿TA的公开信息
+                      wx.getUserInfo({
+                        fail: res => {
+                          console.log('用户拒绝授权获取TA的公开信息');
+                        },
+                        success: res => {
+                          // 获取授权成功后再打印一下授权列表确认
+                          wx.getSetting({
+                            success: res => {
+                              console.log(`好了，用户授权获取TA的公开信息，权限属性 scope.userInfo: %c${res.authSetting[`scope.userInfo`]}`,`color: #ffff; background-color: #113c6d;`);
+                            }
+                          });
+                          // 将全局的 userInfo 赋值为刚拿回来的 userInfo
+                          this.globalData.userInfo = res.userInfo
+                          // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                          // 所以此处加入 callback 以防止这种情况
+                          if (this.userInfoReadyCallback) {
+                            this.userInfoReadyCallback(res)
+                          }
+                        }
+                      })
+                    }
+                    else {
+                      console.log(`用户已经授权获取TA的公开信息，权限属性 scope.userInfo: %c${res.authSetting[`scope.userInfo`]}`,`color: #ffff; background-color: #113c6d;`);
+                    }
+                  }
+                })
+              }
             },
+            // 访问第三方登录接口不成功
             fail: res => {
-              console.log(`执行重新登录失败，返回信息为 %c${res.errMsg}`,`color: #e06c75;`);
-              console.log(`执行重新登录失败，接口状态码为 %c${res.statusCode}`,`color: #e06c75;`);
+              console.log(`访问第三方登录接口不成功，返回信息为 %c${res.errMsg}`,`color: #e06c75;`);
+              console.log(`访问第三方登录接口不成功，接口状态码为 %c${res.statusCode}`,`color: #e06c75;`);
             },
+            // 访问第三方登录接口完成
             complete: res => {
-              console.log(`登录接口请求执行完成`);
-            }             
+              console.log(`访问第三方登录接口完成`);
+            }
           })
-        } else {
+        }
+        // 如果接口返回的数据里的 code 不存在，则在控制台打印出信息
+        else {
           console.log(`用户登录凭证（code）不存在！返回信息为 %c${res.errMsg}`,`color: #e06c75;`)
         }
-        // 进来先验证登录状态
-        // if (res.errMsg == 'login:ok') {
-        //   console.log('登录成功');
-        //   console.log(`并从微信服务端拿到用户的code(有效期五分钟): %c${res.code}`, `color: #ff9600;`);
-
-          // 获取用户授权，获取TA的公开信息
-          // wx.getUserInfo({
-          //   fail: console.log('但用户还没授权应用获取用户公开信息'),
-          //   success: res => {
-          //     console.log('好了，用户授权获取TA的公开信息了'),
-          //       console.log('现在打印出用户的公开信息：', res.userInfo),
-
-          //       // 将全局的userInfo赋值为刚拿回来的userInfo
-          //       this.globalData.userInfo = res.userInfo
-          //     // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-          //     // 所以此处加入 callback 以防止这种情况
-          //     if (this.userInfoReadyCallback) {
-          //       this.userInfoReadyCallback(res)
-          //     }
-          //   }
-          // })
-
-        //   // wx.getSetting({
-        //   //   success: res => {
-        //   //     console.log('getSettingRes', res);
-        //   //     // if (res.authSetting['scope.userInfo']) {
-        //   //     //   // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-        //   //     //   wx.getUserInfo({
-        //   //     //     success: res => {
-        //   //     //       // 可以将 res 发送给后台解码出 unionId
-        //   //     //       this.globalData.userInfo = res.userInfo
-
-        //   //     //       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-        //   //     //       // 所以此处加入 callback 以防止这种情况
-        //   //     //       if (this.userInfoReadyCallback) {
-        //   //     //         this.userInfoReadyCallback(res)
-        //   //     //       }
-        //   //     //     }
-        //   //     //   })
-        //   //     // } else {
-        //   //     //   console.log('用户拒绝授权');
-        //   //     // }
-        //   //   }
-        //   // })
-        // } else {
-        //   console.log('登录失败');
-        // }
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        // wx.getUserInfo({
-        //   success: res => {
-        //     // console.log('userInfo',res.userInfo);
-        //     // app.globalData.usedrInfo = res.userInfo
-        //     // this.setData({
-        //     //   userInfo: res.userInfo,
-        //     //   hasUserInfo: true
-        //     // })
-        //   }
-        // })
       },
+      // 访问微信的 login 接口不成功
       fail: res => {
-        console.log(`登录请求失败，返回信息为 %c${res.errMsg}`,`color: #e06c75;`);
+        console.log(`使用 we.login 不成功，返回信息为 %c${res.errMsg}`,`color: #e06c75;`);
+        console.log(`使用 we.login 不成功，接口状态码为 %c${res.statusCode}`,`color: #e06c75;`);
       },
+      // 访问微信的 login 接口完成
       complete: res => {
-        console.log(`登录请求执行完成`);
-      }  
+        console.log(`使用 we.login 完成`);
+      }
     })
-
-    // 检测用户登录态是否失效
-    // wx.checkSession({
-    //   // 登录状态未失效
-    //   success: res => {
-    //     console.log(`用户登录状态并未失效，返回信息为 %c${res.errMsg}`,`color: #6ab796;`);
-    //     wx.getStorage({
-    //       key: 'session_key',
-    //       success: res => {
-    //           console.log(`getStorageRes`,res);
-    //       } 
-    //     })
-    //   },
-    //   // 登录状态已经失效
-    //   fail: res => {
-    //     console.log(`用户登录状态已经失效，返回信息为 %c${res.errMsg}`,`color: #e06c75;`);
-    //     // 重新登录
-    //     console.log(`现在进行重新登录`);
-    //   }
-    // })
-
-    
-
-    // 获取用户信息
-    // wx.getSetting({
-    //   success: res => {
-    //     console.log('getSettingRes',res);
-    //     if (res.authSetting['scope.userInfo']) {
-    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-    //       wx.getUserInfo({
-    //         success: res => {
-    //           // 可以将 res 发送给后台解码出 unionId
-    //           this.globalData.userInfo = res.userInfo
-
-    //           // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-    //           // 所以此处加入 callback 以防止这种情况
-    //           if (this.userInfoReadyCallback) {
-    //             this.userInfoReadyCallback(res)
-    //           }
-    //         }
-    //       })
-    //     } else {
-    //       console.log('用户拒绝授权');
-    //     }
-    //   }
-    // })
   },
 
   globalData: {
