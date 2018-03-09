a563545a3180ed20dfb583cf718e5aa986e6c575
ray <alien1983gs@126.com>
2018-03-09 13:57:37 +0800

    修改了一下打印信息的样式

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ App({
 
     // 展示用户当前存储在本地的第三方 session_key
     const local_session_key = wx.getStorageSync(`local_session_key`);
-    console.log(`用户当前存储在本地的第三方 session_key（local_session_key）: %c${local_session_key}`,`color: #ffff; background-color: #113c6d;`);
+    console.log(`用户当前存储在本地的第三方 session_key（local_session_key）: %c${local_session_key}`,`color: #ffb119; background-color: transparent;`);
 
     // 进入小程序就让用户登录
     wx.login({
       // 访问微信的 login 接口成功后就开始执行登录逻辑
       success: res => {
         // 开始执行登录逻辑，请求微信服务器拿用户的 code (有效期五分钟)
-        console.log(`使用 we.login 从微信拿到用户的code: %c${res.code}`,`color: #ffff; background-color: #113c6d;`);
+        console.log(`使用 we.login 从微信拿到用户的code: %c${res.code}`,`color: #ffb119; background-color: transparent;`);
         // 如果接口返回的数据里的 code 存在，就开始访问第三方服务器的登录接口
         if (res.code) {
           // 请求第三放服务器的登录接口
@@ -32,7 +32,7 @@ App({
             // 成功访问第三方登录接口后会拿到重新组装过的 session_key
             success: res => {
               // 如果接口返回的 session_key 存在，就开始完成二步登录业务逻辑
-              console.log(`第三方返回重组完成的 session_key: %c${res.data.session_key}`,`color: #ffff; background-color: #113c6d;`);
+              console.log(`第三方返回重组完成的 session_key: %c${res.data.session_key}`,`color: #ffb119; background-color: transparent;`);
               if (res.data.session_key) {
                 // 第一步: 同步存储拿到的第三方 session_key 到 localStorage，命名为 local_session_key
                 try {
@@ -42,7 +42,7 @@ App({
                   )
                   // 存储成功后将 local_session_key 在控制台打印出来
                   const local_session_key = wx.getStorageSync(`local_session_key`);
-                  console.log(`执行 wx.setStorageSync 成功，local_session_key 变更为: %c${local_session_key}`,`color: #ffff; background-color: #113c6d;`);
+                  console.log(`执行 wx.setStorageSync 成功，local_session_key 变更为: %c${local_session_key}`,`color: #ffb119; background-color: transparent;`);
                 }
                 catch (e) {
                   console.log(`同步存储 session_key 到本地 local_session_key 时出错`,e);
@@ -52,7 +52,7 @@ App({
                   // 访问成功后，如果返回的数据里 scope.userInfo（公开信息）不存在则重新获取用户授权
                   success: res => {
                     if (!res.authSetting['scope.userInfo']) {
-                      console.log(`用户并未授权获取TA的公开信息，权限属性 scope.userInfo: %c${res.authSetting[`scope.userInfo`]}`,`color: #ffff; background-color: #113c6d;`);
+                      console.log(`用户并未授权获取TA的公开信息，权限属性 scope.userInfo: %c${res.authSetting[`scope.userInfo`]}`,`color: #ffb119; background-color: transparent;`);
                       // 获取用户授权拿TA的公开信息
                       wx.getUserInfo({
                         fail: res => {
@@ -62,7 +62,7 @@ App({
                           // 获取授权成功后再打印一下授权列表确认
                           wx.getSetting({
                             success: res => {
-                              console.log(`好了，用户授权获取TA的公开信息，权限属性 scope.userInfo: %c${res.authSetting[`scope.userInfo`]}`,`color: #ffff; background-color: #113c6d;`);
+                              console.log(`好了，用户授权获取TA的公开信息，权限属性 scope.userInfo: %c${res.authSetting[`scope.userInfo`]}`,`color: #ffb119; background-color: transparent;`);
                             }
                           });
                           // 将全局的 userInfo 赋值为刚拿回来的 userInfo
@@ -76,7 +76,7 @@ App({
                       })
                     }
                     else {
-                      console.log(`用户已经授权获取TA的公开信息，权限属性 scope.userInfo: %c${res.authSetting[`scope.userInfo`]}`,`color: #ffff; background-color: #113c6d;`);
+                      console.log(`用户已经授权获取TA的公开信息，权限属性 scope.userInfo: %c${res.authSetting[`scope.userInfo`]}`,`color: #ffb119; background-color: transparent;`);
                     }
                   }
                 })
